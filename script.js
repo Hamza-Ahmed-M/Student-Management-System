bcda681ec98b0045dc76167493596dce5eeb8f5b
Hamza Ahmed <hamza.ahmed.memon69@gmail.com>
2026-07-11 16:54:42 +0500

    Login page created

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-let x=document.getElementById("paragraph1")
-console.dir(x)
+let loginButton=document.getElementById("loginbutton");
+addEventListener("click",loginpage);
 
-let button=document.getElementById("loginbutton")
-button.addEventListener("click", changecolor)
-
-function changecolor(){
-    let y=document.getElementsByClassName("first")
-    y[0].style.color="purple";
-    y[1].style.color="purple";
+function loginpage(){
+    window.location.href="loginpage.html";    
 }
